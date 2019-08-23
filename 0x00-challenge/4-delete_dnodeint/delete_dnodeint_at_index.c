a35cee4e83972dc07e547f0460d3fa75ce535dc2
andresmartin <andresmartin266@gmail.com>
2019-08-23 20:00:01 +0000

    fix code doubly linked list

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -31,17 +31,17 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (1);
 	}
 	p = 0;
-	while (p < index && tmp)
+	while (tmp && p < index)
 	{
 		tmp = tmp->next;
 		p++;
 	}
-
 	if (!tmp)
 		return (-1);
 	if (tmp->prev)
 		tmp->prev->next = tmp->next;
 	if (tmp->next)
 		tmp->next->prev = tmp->prev;
+	free(tmp);
 	return (1);
 }
