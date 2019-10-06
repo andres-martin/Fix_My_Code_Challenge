4a0ef0d82a31392d49dd4b3c4c52e29009e2132a
andres-martin <andresmartin266@gmail.com>
2019-10-06 04:54:05 +0000

    [add] setters and getters added

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -5,12 +5,39 @@
 class Square:
     ''' Square class '''
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, width=0, height=0):
         ''' initializer for attributes '''
-        self.width = 0
-        self.height = 0
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+        self.width = width
+        self.height = height
+
+    def __wh_validator(self, name, value):
+        ''' comments '''
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be > 0".format(name))
+
+    @property
+    def width(self):
+        ''' comments '''
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        ''' comments '''
+        self.__wh_validator("width", value)
+        self.__width = value
+
+    @property
+    def height(self):
+        ''' comments '''
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        ''' comments '''
+        self.__wh_validator("height", value)
+        self.__height = value
 
     def area_of_my_square(self):
         """ Area of the square """
