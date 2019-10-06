846818b2b0a956234df04a87954dfb31878c0666
andres-martin <andresmartin266@gmail.com>
2019-10-06 04:30:25 +0000

    [fix] pep8 style

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -20,6 +20,7 @@ class Square:
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        ''' documentation '''
         return "{}/{}".format(self.width, self.height)
 
 
